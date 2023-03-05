a070f8d606c3d4388ea2a1c4a23e109f8b6ad8b9
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-03-04 20:39:51 -0800

    Added OpenAI API and Working

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_KEY")
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -16,23 +16,14 @@ tree = app_commands.CommandTree(client)
 @tree.command(name = "gpt", description = "Ask ChatGPT Anything!")
 
 async def first_command(interaction, message: str):
-    response = openai.Completion.create(model="text-davinci-003", prompt=message, temperature=0, max_tokens=7)
-    await interaction.response.send_message("Hello!" + "\nResponse: " + response, ephemeral = True)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": message}])
+    print("User: \n" + message)
+    print("Bot: \n" + response['choices'][0]['message']['content'].strip())
+    await interaction.response.send_message("Prompt: " + message + response['choices'][0]['message']['content'], ephemeral = True)
 
 @client.event
 async def on_ready():
     await tree.sync()
     print(f'{client.user} has connected to Discord!')
-
-
-# openai.ChatCompletion.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#         {"role": "system", "content": "You are a helpful assistant."},
-#         {"role": "user", "content": "Who won the world series in 2020?"},
-#         {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-#         {"role": "user", "content": "Where was it played?"}
-#     ]
-# )
 
 client.run(TOKEN)
